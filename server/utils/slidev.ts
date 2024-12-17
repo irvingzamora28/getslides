88962318ac23de4e2b7f5260f8c76b5e9d95091f
Irving Zamora <irvingzamora28@gmail.com>
2024-12-17 07:18:28 -0700

    chore: Update .gitignore and refactor presentation generation paths - Modify slidev script to avoid unnecessarily copying files into.output/public folder

--- server/utils/slidev.ts
+++ server/utils/slidev.ts
@@ -6,21 +6,15 @@ import { existsSync } from 'fs'
 export async function generateSlidevPresentation(id: string, content: string) {
   console.log('Starting Slidev presentation generation...');
 
-  let publicPublicDir = ""
 
   // Ensure presentations directory exists
   const presentationDir = join(process.cwd(), 'storage', 'presentations', id);
   console.log('Presentation directory:', presentationDir);
 
-  const distDir = join(process.cwd(), 'tmp', 'dist', id);
+  const distDir = join(presentationDir, 'dist');
   const publicDistDir = join(process.cwd(), 'public', 'presentations', id);
 
   console.log("Current directory:", process.cwd());
-  // If the current directory is .output, create a public directory outside it, meaning ../public
-  
-  if (basename(process.cwd()) === '.output') {
-    publicPublicDir = join(process.cwd(), '..', 'public', 'presentations', id);
-  }
 
   await mkdir(presentationDir, { recursive: true });
   console.log('Created presentation directory if it did not exist.');
@@ -31,14 +25,8 @@ export async function generateSlidevPresentation(id: string, content: string) {
   await mkdir(publicDistDir, { recursive: true });
   console.log('Created public distribution directory if it did not exist.');
 
-  // if publicPublicDir is not empty
-  if (publicPublicDir) {
-    await mkdir(publicPublicDir, { recursive: true });
-    console.log('Created public public directory if it did not exist.');
-  }
-
   // Create slides.md file
-  const slidesPath = join(process.cwd(), 'tmp', `${id}.md`);
+  const slidesPath = join(presentationDir, 'slides.md');
   await writeFile(slidesPath, content);
   console.log('Written slides.md file at:', slidesPath);
 
@@ -69,12 +57,6 @@ export async function generateSlidevPresentation(id: string, content: string) {
     console.log('Attempting to copy files from distDir:', distDir);
     console.log('Attempting to copy files to publicDistDir:', publicDistDir);
     await cp(distDir, publicDistDir, { recursive: true });
-    // Copy generated files to public directory
-    if (publicPublicDir) {
-      console.log('Attempting to copy files from distDir:', distDir);
-      console.log('Attempting to copy files to publicPublicDir:', publicPublicDir);
-      await cp(distDir, publicPublicDir, { recursive: true });
-    }
 
     return {
       slidesPath,
