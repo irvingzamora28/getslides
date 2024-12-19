a04ca96a775e81aa8f301400ef902bf706213420
Irving Zamora <irvingzamora28@gmail.com>
2024-12-19 02:13:49 -0700

    feat: Improve slide content generation guidelines with detailed formatting and interactive elements in openai script
    - Refine instructions for Slidev markdown formatting
    - Introduce transitions and layouts for dynamic presentations

--- server/utils/openai.ts
+++ server/utils/openai.ts
@@ -48,10 +48,87 @@ export async function generateSlideContent(prompt: string) {
         3. Use Markdown formatting for emphasis
         4. Structure content with clear hierarchy
         5. Include code snippets if relevant
-        6. Add image placeholders if needed
-        7. Format as proper Slidev markdown
-        8. Do not wrap the content in markdown code blocks
-        9. Do not use image placeholders
+        6. Format as proper Slidev markdown
+        7. Do not wrap the content in markdown code blocks
+        8. Do not use image placeholders
+        9. Do not use "Slide No X" as title
+        10. Only use code snippets when is a software related presentation
+        11. Include interactive content with 'v-click' animations.
+        12. When you use a <div> element, inside you MUST only use html elements, no markdown formatting.
+        13. Use transitions to create a dynamic and engaging presentation.
+        14. Only use <div> elements when you use 'v-click', otherwise use normal markdown.
+        15. Where you use markdown use ** to highlight important words or concepts.
+        16. Use either of these layouts on each slide: (default, center, fact, full, two-cols, two-cols-header). Use them interchangably depending on the content.
+
+        The 'v-click' elements are used to display elements at a time, giving the users the impression of an interactive presentation.
+        Use 'v-clik' when you list items (But do not overuse in all slidse).
+        You can add \`v-click\` to elements to add a click animation.
+
+        The following example shows how to use \`v-click\`:
+
+        These are the elements that will be animated:
+        <div v-click>
+          <b>Element 1</b> Some more text.
+        </div>
+        <div v-click>
+          <b>Element 2</b> Some more text.
+        </div>
+        <div v-click>
+          <b>Element 3</b> Some more text.
+        </div>
+        <div v-click>This shows up when you click the slide.</div>
+
+        The transitions are placed in the beginning of each slide in the following format:
+        ---
+        transition: fade-out
+        ---
+
+        The layout is placed in the beginning of each slide after the transitions in the following format:
+        ---
+        transition: fade-out
+        layout: center
+        ---
+
+        The two-cols and two-cols-header layouts are used as follows:
+
+        ---
+        layout: two-cols
+        ---
+
+        # Left
+
+        This shows on the left
+
+        ::right::
+
+        # Right
+
+        This shows on the right
+
+        ---
+        layout: two-cols-header
+        ---
+
+        # Title
+
+        ::left::
+
+        # Left subtitle
+
+        This shows on the left
+
+        ::right::
+
+        # Righ subtitle
+
+        This shows on the right
+
+        The speaker notes are placed at the end of each slide in the following format:
+        <!--
+        Note 1
+        Note 2
+        -->
+        ---
         `
       },
       {
